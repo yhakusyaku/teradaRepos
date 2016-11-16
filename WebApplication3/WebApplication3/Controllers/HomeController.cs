c702e8a8e5e7ca86badfba13587d5301b1182a96
solTerada <terada@solekia.co.jp>
2016-11-16 22:49:53 +0900

    commit message

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // Get: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
